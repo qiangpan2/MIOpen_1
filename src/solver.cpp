d7ef26f4a4eda6bb35ab5589d7bfa9bde700def3
qiang <qiangpan@amd.com>
2025-09-03 17:47:00 +0800

    remove mha ck solver under flag

--- src/solver.cpp
+++ src/solver.cpp
@@ -706,7 +706,9 @@ inline SolverRegistrar::SolverRegistrar(IdRegistryData& registry)
              Primitive::MultiMarginLoss,
              multimarginloss::MultiMarginLossForward{}.SolverDbId());
 
+#if MIOPEN_USE_COMPOSABLEKERNEL
     Register(registry, ++id, Primitive::Mha, mha::MhaCKFlashAttentionV2Forward{}.SolverDbId());
+#endif
     // IMPORTANT: New solvers should be added to the end of the function, and don't leave a white
     // space between this comment and the newly registered solver(s)!
 }
