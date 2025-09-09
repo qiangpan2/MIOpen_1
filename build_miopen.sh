ba87d684b4abc92ba57f32efb001a1d1cb3ded2e
qiangpan <qiangpan@amd.com>
2025-09-09 10:00:29 +0000

    setting LD_LIBRARY

--- build_miopen.sh
+++ build_miopen.sh
@@ -20,7 +20,8 @@ cmake -B build \
 # 构建项目
 cmake --build build -j8 > build.log 2>&1
 
-./build/test/test_conv3d_channel_last_wmmaops --half
+LD_LIBRARY_PATH=/workspace/repo/MIOpen_1/build/lib:$LD_LIBRARY_PATH
+./build/bin/test_conv3d_channel_last_wmmaops 
 
 # 安装项目
 echo "Installing MIOpen..."
